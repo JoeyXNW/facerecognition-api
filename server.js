3d86009cf215da1524120077a1b03839bc90efe6
xwang985 <39933823+xwang985@users.noreply.github.com>
2020-01-20 18:33:16 -0500

    change PORT

--- server.js
+++ server.js
@@ -29,6 +29,6 @@ app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcryp
 app.get('/profile/:id', (req, res) => {profile.handleProfile(req, res)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res, db)})
-app.listen(3000, ()=> {
-	console.log('app is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+	console.log(`app is running on port ${process.env.PORT}`);
 })
